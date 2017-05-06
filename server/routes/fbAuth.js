7a5e9e8ca7d46159b9b3141a543e7399d495ad1b
Bhirawa Mbani <bhirmbani@gmail.com>
2017-05-06 23:00:43 +0700

    refactoring and adding button logout from fb

--- server/routes/fbAuth.js
+++ server/routes/fbAuth.js
@@ -3,10 +3,13 @@ const router = express.Router();
 const passport = require('passport');
 const fbAuth = require('../controllers/fbAuth');
 
-router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }), function(req, res) {
+  res.json({user: req.user});
+});
+
 router.get('/auth/facebook/callback', passport.authenticate('facebook', {
   successRedirect: 'http://127.0.0.1:8080/',
   failureRedirect: 'http://127.0.0.1:8080/'
-}))
+}));
 
 module.exports = router;
